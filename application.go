3949f4d129edd2aab86a2ae0ef1a1cab1db6d5fd
Corbin Crutchley <crutchcorn@gmail.com>
2021-08-17 18:36:09 -0700

    Initial work on seek logic

--- application.go
+++ application.go
@@ -6,24 +6,25 @@ import (
 	"os"
 )
 
-//func drawText(s tcell.Screen, x1, y1 int, style tcell.Style, text string) {
-//	row := y1
-//	col := x1
-//	for _, r := range []rune(text) {
-//		s.SetContent(col, row, r, nil, style)
-//		col++
-//	}
-//}
-
-func main() {
-	// TODO: Replace this with file selector
-	dat, e := os.Open("demo.txt")
-	bytes := make([]byte, 500)
-	numOfBytes, err := dat.Read(bytes)
-
+func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func main() {
+	var bytesPerLine int64 = 20
+	numberOfRows := 3
+
+	// TODO: Replace this with file selector
+	dat, e := os.Open("demo.txt")
+	check(e)
+	bytes := make([]byte, bytesPerLine * int64(numberOfRows))
+	numOfBytes, err := dat.ReadAt(bytes, 0)
+	check(err)
+	fileStat, statE := dat.Stat()
+	fileSize := fileStat.Size()
+	check(statE)
 
 	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
 	textStyle := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorPurple)
@@ -39,21 +40,23 @@ func main() {
 	s.SetStyle(defStyle)
 	s.Clear()
 
-	currentRow := 0
-	bytesPerLine := 20
-	var maxRowIndex int = numOfBytes / bytesPerLine
+	var currentRow int64 = 0
 
 	redraw := func() {
-		s.Clear()
+		numOfOffsetBytes, err := dat.ReadAt(bytes, currentRow * bytesPerLine)
+		numOfBytes = numOfOffsetBytes
+		check(err)
 
-		for row := 0; row < 2; row++ {
-			rowIndex := currentRow + row
-			for col := 0; col < bytesPerLine; col++ {
-				byteIndex := (rowIndex * bytesPerLine) + col
-				if byteIndex >= numOfBytes {
+		s.Clear()
+		for row := 0; row < numberOfRows; row++ {
+			rowIndex := currentRow + int64(row)
+			for col := 0; int64(col) < bytesPerLine; col++ {
+				byteTotalIndex := (rowIndex * bytesPerLine) + int64(col)
+				byteLocalIndex := (int64(row) * bytesPerLine) + int64(col)
+				if byteTotalIndex >= fileSize {
 					break
 				}
-				b := bytes[byteIndex]
+				b := bytes[byteLocalIndex]
 				s.SetContent(col, row, rune(b), nil, textStyle)
 			}
 		}
@@ -89,6 +92,7 @@ func main() {
 				redraw()
 			} else if ev.Key() == tcell.KeyDown {
 				currentRow += 1
+				maxRowIndex := fileSize / bytesPerLine
 				if currentRow >= maxRowIndex {
 					currentRow = maxRowIndex
 				}
