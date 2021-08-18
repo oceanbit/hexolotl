4c1e2b910644417bfdda2c24c02a7eda06b9a4e8
Corbin Crutchley <crutchcorn@gmail.com>
2021-08-17 17:04:03 -0700

    Added POC of CLI util

---
+++ application.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"github.com/gdamore/tcell/v2"
+	"log"
+	"os"
+)
+
+func drawText(s tcell.Screen, x1, y1 int, style tcell.Style, text string) {
+	row := y1
+	col := x1
+	for _, r := range []rune(text) {
+		s.SetContent(col, row, r, nil, style)
+		col++
+	}
+}
+
+func main() {
+	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
+	boxStyle := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorPurple)
+
+	// Initialize screen
+	s, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+	if err := s.Init(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+	s.SetStyle(defStyle)
+	s.Clear()
+
+	// Draw initial boxes
+	drawText(s, 0, 0, boxStyle, "Click and drag to draw a box")
+	drawText(s, 0, 1, boxStyle, "Press C to reset")
+
+	// Event loop
+	quit := func() {
+		s.Fini()
+		os.Exit(0)
+	}
+	for {
+		// Update screen
+		s.Show()
+
+		// Poll event
+		ev := s.PollEvent()
+
+		// Process event
+		switch ev := ev.(type) {
+		case *tcell.EventResize:
+			s.Sync()
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
+				quit()
+			} else if ev.Key() == tcell.KeyUp {
+				s.Sync()
+			} else if ev.Key() == tcell.KeyDown {
+				s.Clear()
+			}
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/oceanbit/hex
+
+go 1.16
+
+require github.com/gdamore/tcell/v2 v2.4.0
